0eca28f9b489630c74d8fd1cec6acdfa797b3fe5
Isadora Hoelscher <isadorahoelscher@gmail.com>
2023-04-05 02:18:54 -0300

    :lipstick: adicionando scroll infinito, pagina singular de foto e estilizando a pag de fotos

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React from 'react';
-import './app.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import { UserStorage } from './UserContext';
-import User from './Components/User/User';
-import ProtectedRoute from './Components/Helper/ProtectedRoute';
+import React from "react";
+import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import Photo from "./Components/Photo/Photo";
 
 const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<UserStorage>
-					<Header />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="login/*" element={<Login />} />
-						<Route
-							path="conta/*"
-							element={
-								<ProtectedRoute>
-									<User />
-								</ProtectedRoute>
-							}
-						/>
-					</Routes>
-					<Footer />
-				</UserStorage>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="foto/:id" element={<Photo />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
