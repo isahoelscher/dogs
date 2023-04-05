ec4b3d3eb58162e6fa87bbcacd68b7f48f68a9a8
Isadora Hoelscher <isadorahoelscher@gmail.com>
2023-04-05 05:01:04 -0300

    finalizando a aplicação

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -8,15 +8,17 @@ import styles from "./FeedPhotos.module.css";
 
 const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
+
   React.useEffect(() => {
     async function fetchPhotos() {
-      const total = 3;
+      const total = 6;
       const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
       if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
   }, [request, user, page, setInfinite]);
+
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
   if (data)
